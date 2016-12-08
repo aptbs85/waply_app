32835d01be7366665d035ad5e1816a2c870529bc
John <jzamparas@gmail.com>
2016-12-08 21:04:31 +0000

    button captions form

--- app/controllers/products/new.js
+++ app/controllers/products/new.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+        addProduct: function(){
+            var productPhotoDescription = this.get('productPhotoDescription');
+            var supplyAbility = this.get('supplyAbility');
+            var minimumOrder = this.get('minimumOrder');
+            var certifications = this.get('certifications');
+            var placeOfOgirin = this.get('placeOfOgirin');
+            var productDescription = this.get('productDescription');
+            var packagingDetails = this.get('packagingDetails');
+            var pickupAddress = this.get('pickupAddress');
+            var productLifeCycle = this.get('productLifeCycle');
+            var pricingPolicy = this.get('pricingPolicy');
+        }
+    }
 });
