24259de2c6fa51e56c59974459e842f713cf7808
John <jzamparas@gmail.com>
2016-12-08 21:27:09 +0000

    installed emberfire

---
+++ app/models/product.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    productPhotoDescription: DS.attr('string')
+    supplyAbility: DS.attr('string')
+    minimumOrder: DS.attr('string')
+    certifications: DS.attr('string')
+    placeOfOgirin: DS.attr('string')
+    productDescription: DS.attr('string')
+    packagingDetails: DS.attr('string')
+    pickupAddress: DS.attr('string')
+    productLifeCycle: DS.attr('string')
+    pricingPolicy: DS.attr('string')
+});
